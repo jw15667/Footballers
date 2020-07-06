9a27f84606135b054f957bad6e3513fadaafbbe3
jw15667 <63715754+jw15667@users.noreply.github.com>
2020-07-06 09:39:12 +0100

    Add files via upload

---
+++ scrapping.py
@@ -0,0 +1,50 @@
+from bs4 import BeautifulSoup
+import requests
+import csv
+
+club_list = ['arsenal', 'aston-villa', 'barnsley', 'birmingham-city', 'blackburn-rovers', 'blackpool',
+             'bolton-wanderers', 'afc-bournemouth', 'bradford-city', 'brighton-and-hove-albion', 'burnley',
+             'cardiff-city', 'charlton-athletic', 'chelsea', 'coventry-city', 'crystal-palace', 'derby-county',
+             'everton', 'fulham', 'huddersfield-town', 'hull-city', 'ipswich-town', 'leeds-united', 'leicester-city',
+             'liverpool', 'manchester-city', 'manchester-united', 'middlesbrough', 'newcastle-united', 'norwich-city',
+             'nottingham-forest', 'oldham-athletic', 'portsmouth', 'queens-park-rangers', 'reading', 'sheffield-united',
+             'sheffield-wednesday', 'southampton', 'stoke-city', 'sunderland', 'swansea-city', 'swindon-town', 'tottenham-hotspur',
+             'watford', 'west-bromwich-albion', 'west-ham-united', 'wigan-athletic', 'wimbledon', 'wolverhampton-wanderers']
+season = ['1992', '1993', '1994', '1995', '1996', '1997', '1998', '1999', '2000', '2001', '2002', '2003', '2004', '2005',
+          '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019']
+
+
+team_season = []
+for club, year in [(club, year) for club in club_list for year in season]:
+    team = dict()
+    website = 'https://www.11v11.com/teams/{}/tab/players/season/{}/'.format(club, year)
+    team['Club'] = "{}".format(club)
+    team['Year'] = "{}".format(year)
+    team_season.append(team)
+
+
+
+    source = requests.get(website).text
+
+    soup = BeautifulSoup(source, 'lxml')
+
+    squad = soup.find(class_="squad sortable").find("tbody").find_all("tr")
+
+    for rows in squad:
+        cells = rows.find_all("td")
+        player_name = cells[1].get_text()
+        team['Player'] = "{}".format(player_name)
+        print(team)
+        team_season.append(player_name)
+
+csv_columns = ['No','Club','Year', 'Player']
+csv_file = "Football_squads.csv"
+try:
+    with open(csv_file, 'w') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
+        writer.writeheader()
+        for data in team_season:
+                writer.writerow(data)
+except IOError:
+    print("I/O error")
+
