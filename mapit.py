4b85b2c53c2b3034901deb7c86c61fa92d43f273
jw15667 <63715754+jw15667@users.noreply.github.com>
2020-10-13 20:31:18 +0100

    Update mapit.py

--- mapit.py
+++ mapit.py
@@ -6,21 +6,22 @@ player_id_to_team_ids_map = defaultdict(list)
 team_id_to_player_name = defaultdict(list)
 
 with open('Football_squads.csv', 'r') as read_obj:
-    csv_reader = reader(read_obj)
-    for row in csv_reader:
-        player_name = row[2]
-        player_id = row[3]
-        team_id = row[4]
-        player_name_to_player_id_map[player_name] = player_id
-        player_id_to_team_ids_map[player_id].append(team_id)
-        team_id_to_player_name[team_id] = player_name
+        csv_reader = reader(read_obj)
+        for row in csv_reader:
+            player_name = row[2]
+            player_id = row[3]
+            team_id = row[4]
+            player_name_to_player_id_map[player_name] = player_id
+            player_id_to_team_ids_map[player_id].append(team_id)
+            team_id_to_player_name[team_id].append(player_name)
 
 
 searched_club_ids_1 = set([])
 searched_club_ids_2 = set([])
 
-name_1 = player_name_to_player_id_map.get('nicolas anelka')
-name_2 = player_name_to_player_id_map.get('daniel sturridge')
+
+name_1 = player_name_to_player_id_map.get('gareth barry')
+name_2 = player_name_to_player_id_map.get('ashley young')
 
 if name_1 in player_id_to_team_ids_map:
     search = player_id_to_team_ids_map.get(name_1)
@@ -28,12 +29,14 @@ if name_1 in player_id_to_team_ids_map:
     searched_club_ids_1.update(search)
     searched_club_ids_2.update(add_second_player)
 
-shared_clubs = (searched_club_ids_1.intersection(searched_club_ids_2))
+print(searched_club_ids_1)
 
+shared_clubs = (searched_club_ids_1.intersection(searched_club_ids_2))
+print(shared_clubs)
 total_players = []
 
 for team in shared_clubs:
-    player_list = team_id_to_player_name[team]
+    player_list = team_id_to_player_name.get(team)
     total_players.append(player_list)
 
 print(total_players)
