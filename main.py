4698cfe42c89c61bbb9e3bd0e04f2027137b65c3
sachhamps <sachinhampal0987@gmail.com>
2020-11-12 19:50:18 +0000

    refactored code

--- main.py
+++ main.py
@@ -4,56 +4,25 @@
 import json
 import pprint
 
-import maps
-import player_id_util
-
-def get_team_ids(player_name, player_name_to_player_id, player_id_to_team_ids):
-    player_id = player_name_to_player_id.get(player_name)
-    player_team_ids = player_id_to_team_ids.get(player_id)
-    return player_team_ids
-
-def get_teammate_ids(player_team_ids, player_id_to_team_ids):
-    teammates_ids = set()
-    for player_id, team_ids in player_id_to_team_ids.items():
-        if not player_team_ids.isdisjoint(team_ids):
-            teammates_ids.add(player_id)
-    return teammates_ids
-
-def get_mutual_teammates(mutual_teammates_ids, player_id_to_name_map):
-    teammates = []
-    for player_id in mutual_teammates_ids:
-        teammate = player_id_to_name_map.get(player_id)
-        teammates.append(teammate)
-    return teammates
-
+from consts.player_id_to_name_map import PLAYER_ID_TO_NAME_MAP
+from player import (
+    get_mutual_teammates,
+    Player,
+)
 
 if __name__ == "__main__":
-    squads = player_id_util.complete_squads_dataset()
-    player_id_to_name_map = maps.player_id_to_name(squads)
-    player_name_to_team_ids_map = maps.name_to_team_ids(squads)
-    player_name_to_player_id = maps.player_name_to_ids(squads)
-    player_id_to_team_ids = maps.player_ids_to_team_ids(squads)
-
     print("Enter first player:")
-    player1 = input()
+    first_player_name = input()
     # player1 = "kevin nolan"
 
     print("Enter second player:")
-    player2 = input()
+    second_player_name = input()
     # player2 = "mark noble"
 
-    player1_team_ids = get_team_ids(player1, player_name_to_player_id, player_id_to_team_ids)
-    player2_team_ids = get_team_ids(player2, player_name_to_player_id, player_id_to_team_ids)
+    player1 = Player(first_player_name)
+    player2 = Player(second_player_name)
 
-    player1_teammate_ids = get_teammate_ids(player1_team_ids, player_id_to_team_ids)
-    player2_teammate_ids = get_teammate_ids(player2_team_ids, player_id_to_team_ids)
-
-    mutual_teammate_ids = player1_teammate_ids.intersection(player2_teammate_ids)
-
-    mutual_teammates = get_mutual_teammates(mutual_teammate_ids, player_id_to_name_map)
-
-    if player1 in mutual_teammates: mutual_teammates.remove(player1)
-    if player2 in mutual_teammates: mutual_teammates.remove(player2)
+    mutual_teammates = get_mutual_teammates(player1, player2)
 
     print("mutual teammates are:")
     pprint.pprint(mutual_teammates)
