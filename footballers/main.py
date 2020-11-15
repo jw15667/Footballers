bd396db905ae31814e80f1e529438f25cdbb5f1b
sachhamps <sachinhampal0987@gmail.com>
2020-11-15 15:37:11 +0000

    added twitter fnuctionality

--- footballers/main.py
+++ footballers/main.py
@@ -3,26 +3,38 @@
 """
 import json
 import pprint
+import random
+from retrying import retry
+import sys
 
 from consts.player_id_to_name_map import PLAYER_ID_TO_NAME_MAP
+from consts.player_list import PLAYERS
 from player import (
     get_mutual_teammates,
     Player,
 )
 
-if __name__ == "__main__":
-    print("Enter first player:")
-    first_player_name = input()
-    # player1 = "kevin nolan"
+class Runner:
 
-    print("Enter second player:")
-    second_player_name = input()
-    # player2 = "mark noble"
+    def __init__(self, pl_names):
+        self.pl_names = pl_names
 
-    player1 = Player(first_player_name)
-    player2 = Player(second_player_name)
+    @retry
+    def question(self):
+        self.player_one = Player(random.choice(self.pl_names))
+        self.player_two = Player(random.choice(self.pl_names))
+        self.answer = get_mutual_teammates(self.player_one, self.player_two)
+        answer_length = len(self.answer)
+        if answer_length < 5:
+            raise Exception("No players")
 
-    mutual_teammates = get_mutual_teammates(player1, player2)
+        return f"Can you name all {answer_length} player(s) who have played with {self.player_one.player_name} and {self.player_two.player_name}"
 
-    print("mutual teammates are:")
-    pprint.pprint(mutual_teammates)
+    def answers(self):
+        # print(a for a in self.answer)
+        answer_nb = ', '.join(self.answer)
+        return f"The players who played with {self.player_one.player_name} and {self.player_two.player_name}: {answer_nb}"
+
+# runner = Runner(PLAYERS)
+# runner.question()
+# runner.answers()
